ef78b4e2b72e6a29405c0f9dd183d5bb99995452
Jade 648 <maddiepeterson375@gmail.com>
2022-10-16 13:14:56 -0500

    Updated Ticket File

--- TicketFile.cs
+++ TicketFile.cs
@@ -3,10 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using TicketSystem;
+
 public class TicketFile {
 
-      private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+    private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
     private object scrubbedFile;
+    private ulong ticketId;
+    private string Type;
+    private List<string> description;
+    private string status;
 
     public TicketFile(object scrubbedFile)
     {
@@ -14,6 +20,50 @@ public class TicketFile {
     }
 
     public string filePath {get; set;}
+     public List<TicketSystem.Ticket> Tickets {get; set;}
 
+     public TicketFile(string TicketFilePath, TicketFile ticket)
+    {
+        filePath = TicketFilePath;
+        Tickets = new List<TicketSystem.Ticket>();
+
+        try{
+            StreamReader sr = new StreamReader(filePath);
+            while(!sr.EndOfStream){
+                TicketFile ticketFile = new TicketFile();
+                string line = sr.ReadLine(); 
+
+                int idx = line.IndexOf(",");
+                if (idx == -1)
+                {
+                    string [] ticketDetails = line.Split(',');
+                    ticketFile.ticketId = UInt64.Parse(ticketDetails[0]);
+                    ticketFile.Type = ticketDetails[1];
+                    List<string> list = ticketDetails[2].Split('|').ToList();
+                    ticketFile.description = list;
+                    ticketFile.status = ticketDetails[3];
+
+                }
+                else{
+                    ticketFile.ticketId = UInt64.Parse(line.Substring(0,idx-1));
+                    line = line.Substring (idx);
+                    idx = line.LastIndexOf(",");
+                    ticketFile.Type = line.Substring (0,idx +1);
+                    line = line.Substring(idx +2);
+                    string [] details = line.Split(',');
+                    ticketFile.description = details[0].Split('|').ToList();
+                    ticketFile.status = details [1];
+            
+                }
+
+                Tickets.Add(Ticket);
+            }
+                sr.Close();
+
+                logger.Info("tickets in file {count}",Tickets.Count);
+            }catch (Exception ex){
+            logger.Error(ex.Message);
+        }
+
+    }
 }
-     
