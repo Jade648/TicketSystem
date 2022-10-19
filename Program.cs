8cd135016859c0a3c59bd210a00512c2f7a240c4
Jade 648 <maddiepeterson375@gmail.com>
2022-10-18 19:47:04 -0500

    updated program and tickets files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace TicketSystem
 {
     class Program
     {
+    
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+        private static int ticketId;
+        private static string type;
+        private static object description;
+        private static object status;
+        private static object ticket;
 
         public static object ScrubbedFile { get; private set; }
 
@@ -14,12 +20,21 @@ namespace TicketSystem
         {
             logger.Info("Program started");
 
-           String scrubbedFile = FileScrubber.ScrubTickets("Tickets.csv");
+           String scrubbedFile = FileScrubber.ScrubTickets("Tickets.scrubbed.csv");
 
            logger.Info("Scrubbed file");
 
-           TicketFile ticketFile = new TicketFile(ScrubbedFile);
+           TicketFile ticketFile = new TicketFile(scrubbedFile);
+           
+           {
+            ticketId = 100;
+            type  = "Debug";
+            description = "debugging application";
+            status = "assigned";
+           };   
 
+           Console.WriteLine(ticket.Display());
+           
             logger.Info ("Program ended");
         }
     }
