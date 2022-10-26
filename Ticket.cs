3b35720e3edcf10a02cdb6194956498344fe4ebb
Jade 648 <maddiepeterson375@gmail.com>
2022-10-26 14:19:46 -0500

    Updated Ticket File

--- Ticket.cs
+++ Ticket.cs
@@ -22,7 +22,7 @@ namespace TicketSystem
 
         public virtual string Display()
     {
-            return $"Id:{TicketId}/nstatus:{Status}/n";
+            return $"Id:{TicketId}/n summary: {Summary}/n status:{Status}/n priority: {Priority}/n submitter:{Submitter}/n assigned: {Assigned}/n watching: {Watching}";
         }
     }
 
@@ -31,56 +31,7 @@ namespace TicketSystem
         public string Severity {get; set;}
 
     }
-    //     public string description;
-    //     public string status;
-    
-    //     public Ticket()
-    //     {
-    //         List<string> list = new List<string>();
-           
-    //     }
-
-    //     public string Type { get; internal set; }
-
-    //        public virtual string Display()
-    // {
-    //         return $"Id:{ticketId}/ntype:{type}/ndescription:{description}/nstatus:{string.Join(",",status)}/n";
-    //     }
-
-    //        public class Task
-    // {
-
-    //             public UInt64 ticketId {get; set;}
-
-    //     public string type {get; set;}
-
-    //     public string description {get; set;}
-
-    //     public string status {get; set;}
-
-    //     public string ProjectName {get; set;}
-
-    //     public string DueDate {get; set;}
-
-    //     }   
-
-    //          public class Enhancement
-    // {
-
-    //     public UInt64 ticketId {get; set;}
-    //     public string type {get; set;}
-
-    //     public string description {get; set;}
-
-    //     public string status {get; set;}
-
-    //     public string reason {get; set;}
-
-    // }
-    
-    //     }
-    }
-
+}
 
 
 
